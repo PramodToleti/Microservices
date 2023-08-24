2642047236635da9771b3b28b375a6ba138326bc
PramodToleti <pramodraina047@gmail.com>
2023-08-24 17:37:24 +0530

    Added Add user method

--- src/impl/Users/users.ts
+++ src/impl/Users/users.ts
@@ -40,4 +40,75 @@ export class UserApi implements UserApi {
       }
     })
   }
+
+  getUserById(email: string): Promise<GetUserByIdResponse> {
+    return new Promise<GetUserByIdResponse>(async (resolve, reject) => {
+      try {
+        const isPresent = await collections.users!.findOne({ email })
+        if (isPresent) {
+          const formattedData = {
+            id: isPresent._id,
+            name: isPresent.name,
+            email: isPresent.email,
+            password: isPresent.password,
+            role: isPresent.role,
+            createdAt: isPresent.createdAt,
+            updatedAt: isPresent.updatedAt,
+          }
+          const response: GetUserByIdResponse = {
+            status: 200,
+            body: formattedData,
+          }
+          resolve(response)
+        } else {
+          const response: GetUserByIdResponse = {
+            status: 404,
+            body: {
+              message: "User not found",
+            },
+          }
+          resolve(response)
+        }
+      } catch (error) {
+        /* const response: Api.ApiResponse = {
+          code: 400,
+          message: "Something Went Wrong",
+        } */
+        reject(error)
+      }
+    })
+  }
+
+  addUser(request: Api.User): Promise<AddUserResponse> {
+    return new Promise<AddUserResponse>(async (resolve, reject) => {
+      try {
+        const isPresent = await collections.users!.findOne({
+          email: request.email,
+        })
+        if (isPresent) {
+          const response: AddUserResponse = {
+            status: 400,
+            body: {
+              code: 400,
+              message: "User Already Exists",
+            },
+          }
+          resolve(response)
+        } else {
+          const result = await collections.users!.insertOne(request)
+          const response: AddUserResponse = {
+            status: 200,
+            body: request,
+          }
+          resolve(response)
+        }
+      } catch (err) {
+        /* const response: Api.ApiResponse = {
+          code: 400,
+          message: "Something Went Wrong",
+        } */
+        reject(err)
+      }
+    })
+  }
 }
